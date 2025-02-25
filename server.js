31658a3b08e6409f196bf5e9c90b8126caf7f54c
Étienne Gagnon <etigag2002@gmail.com>
2025-02-25 14:52:21 -0500

    [upd] - visuel front-end

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/views/home.html');
+});
+
+app.get('/add-contact', (req, res) => {
+    res.sendFile(__dirname + '/public/views/add-contact.html');
+});
+
+app.listen(port, () => {
+    console.log('Connected ! \n http://localhost:3000');
+});
