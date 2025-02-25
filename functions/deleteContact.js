e2ccf4919959a0f1b9e4463e1053c0c4e633d820
Étienne Gagnon <etigag2002@gmail.com>
2025-02-25 15:57:12 -0500

    [upd] - deleteContact terminée

--- functions/deleteContact.js
+++ functions/deleteContact.js
@@ -1,8 +1,14 @@
 function deleteContact(userId){
-   let existingUserId = "1";
+   let existingUserId = 1;
 
    if(userId === undefined){
-        return "Le userId n'est pas définis";
+        return "Le userId n'est pas défini";
+   }else if(typeof userId == "string"){
+        return "Le userId n'est pas valide";
+   }else if(userId !== existingUserId){
+        return "Le userId n'existe pas";
+   }else{
+        return "Le contact a été supprimé avec succès";
    }
 }
 
--- test-jest/deleteContact.test.js
+++ test-jest/deleteContact.test.js
@@ -2,5 +2,21 @@ const { deleteContact } = require('../functions/deleteContact.js');
 
 test('Si le userId est undefined', () => {
     // fonction complète qui fonctionnerait: deleteContact(1) 
-    expect(deleteContact()).toBe("Le userId n'est pas définis");
+    expect(deleteContact()).toBe("Le userId n'est pas défini");
+    });
+
+test('Si le userId n\est pas valide', () => {
+    // fonction complète qui fonctionnerait: deleteContact(1) 
+    expect(deleteContact("1")).toBe("Le userId n'est pas valide");
+    });
+
+test('Si le userId n\est pas valide', () => {
+    // fonction complète qui fonctionnerait: deleteContact(1) 
+    expect(deleteContact(2)).toBe("Le userId n'existe pas");
+    });
+
+
+test('Le contact à été supprimé avec succès', () => {
+    // fonction complète qui fonctionnerait: deleteContact(1) 
+    expect(deleteContact(1)).toBe("Le contact a été supprimé avec succès");
     });
