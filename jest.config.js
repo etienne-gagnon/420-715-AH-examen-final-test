4e790ba5a7733845c189591c67b25f6d7a6fbfe1
Étienne Gagnon <etigag2002@gmail.com>
2025-02-25 23:22:46 -0500

    [upd]

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    testPathIgnorePatterns: ['/node_modules/', '\\.spec\\.js$'],
-  };
-  
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '\\.spec\\.js$'],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./reports", outputName: "jest-report.xml" }]
+  ]
+};
